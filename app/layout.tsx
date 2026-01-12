65067cfdd81e2451b3d09cd55f1dd753827fae86
OmorFaruk63 <omor3710@gmail.com>
2026-01-12 13:25:27 +0600

    Add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@
 import Header from "../components/Header";
 import "./globals.css";
 import Providers from "./providers";
-
+import { SpeedInsights } from "@vercel/speed-insights/next";
 export const metadata = {
   title: "Next.js Prisma Full-Stack Blog",
   description: "Blog app with Next.js 15 + Prisma",
@@ -20,6 +20,7 @@ export default function RootLayout({
           <Header />
           {children}
         </Providers>
+        <SpeedInsights />
       </body>
     </html>
   );
